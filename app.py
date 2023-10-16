fbd1e404ac4ba7b0fee37fc39f43bc9b3af1a49c
= <anaskhaaan.28@gmail.com>
2023-10-16 21:30:16 +0530

    add java support ;')

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify,redirect
 from cs50 import SQL
-from compiler import compile_python,compile_cpp
+from compiler import compile_python,compile_cpp,compile_java
 import subprocess
 import time
 import os
@@ -22,10 +22,12 @@ db.execute("""
         test_cases TEXT,
         constraints TEXT,
         boiler_code_python TEXT,
-        boiler_code_cpp TEXT,
-        testing_code TEXT,
         function_call_python TEXT,
-        function_call_cpp TEXT
+        boiler_code_cpp TEXT,
+        function_call_cpp TEXT,
+        boiler_code_java TEXT,
+        function_call_java TEXT,
+        testing_code TEXT
     )
 """)
 
@@ -58,128 +60,89 @@ def index():
 @app.route('/create_sample')
 def create_sample():
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python, boiler_code_java, function_call_java)
     VALUES (
         'Sum of Squares',
-        'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of squares of all positive integers from 1 to n inclusive. 
-    
-        Write a program to find this sum using the following formula: 
-    
-        Sum = 1² + 2² + 3² + ... + n²',
-        'Test Case 1: 
-        Input : n = 5
-        Output: 55,
-        Explanation: the first 5 positive integers (1² + 2² + 3² + 4² + 5²) which results in a sum of 55.
-
-        Test Case 2: 
-        Input : n = 8
-        Output: 204',
+        'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of squares of all positive integers from 1 to n inclusive. Write a program to find this sum using the following formula: Sum = 1² + 2² + 3² + ... + n²',
+        'Test Case 1: Input : n = 5\nOutput: 55\nExplanation: the first 5 positive integers (1² + 2² + 3² + 4² + 5²) which results in a sum of 55.\n\nTest Case 2: Input : n = 8\nOutput: 204',
         '1 ≤ n ≤ 1000',
         'def square(n):\n    # return the answer',
         '#include <iostream>\nusing namespace std;\n\nint square(int n) {\n    // return the answer\n}',
         'def square(n):\n    total = 0\n    for i in range(1, n + 1):\n        total += i ** 2\n    return total',
         'int main() {\n    int n = 5;\n    cout << square(n) << endl;\n    n = 8;\n    cout << square(n) << endl;\n    return 0;\n}',
-    'n = 5\n
-print(square(n))\n
-n = 8\n
-print(square(n))'
-);
+        'n = 5\nprint(square(n))\nn = 8\nprint(square(n))',
+        'public class Solution {\n    public static int square(int n) {\n        // return the answer\n    }\n}',
+        'public static void main(String[] args) {\n    int n = 5;\n    System.out.println(Solution.square(n));\n    n = 8;\n    System.out.println(Solution.square(n));\n}\n}'
+    );
 """)
 
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python, boiler_code_java, function_call_java)
     VALUES (
         'Sum of Integers',
-        'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of all positive integers from 1 to n inclusive. 
-        
-        Write a program to find this sum using the following formula: 
-        
-        Sum = 1 + 2 + 3 + ... + n',
-        'Test Case 1: 
-        Input: n = 5
-        Output: 15
-        Explanation: The sum of integers from 1 to 5 is 1 + 2 + 3 + 4 + 5, which results in a sum of 15.
-
-        Test Case 2: 
-        Input: n = 8
-        Output: 36',
+        'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of all positive integers from 1 to n inclusive. Write a program to find this sum using the following formula: Sum = 1 + 2 + 3 + ... + n',
+        'Test Case 1: Input: n = 5\nOutput: 15\nExplanation: The sum of integers from 1 to 5 is 1 + 2 + 3 + 4 + 5, which results in a sum of 15.\n\nTest Case 2: Input: n = 8\nOutput: 36',
         '1 ≤ n ≤ 1000',
         'def calculate_sum(n):\n    # return the answer',
         '#include <iostream>\nusing namespace std;\n\nint calculate_sum(int n) {\n    // return the answer\n}',
         'def calculate_sum(n):\n    total = 0\n    for i in range(1, n + 1):\n        total += i\n    return total',
         'int main() {\n    int n = 5;\n    cout << calculate_sum(n) << endl;\n    n = 8;\n    cout << calculate_sum(n) << endl;\n    return 0;\n}',
-        'n = 5\nprint(calculate_sum(n))\nn = 8\nprint(calculate_sum(n))'
+        'n = 5\nprint(calculate_sum(n))\nn = 8\nprint(calculate_sum(n))',
+        'public class Solution {\n    public static int calculateSum(int n) {\n        // return the answer\n    }\n}',
+        'public static void main(String[] args) {\n    int n = 5;\n    System.out.println(Solution.calculateSum(n));\n    n = 8;\n    System.out.println(Solution.calculateSum(n));\n}\n}'
     );
-    """)
+""")
 
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python, boiler_code_java, function_call_java)
     VALUES (
         'Factorial Calculation',
         'You are given a single integer n (0 ≤ n ≤ 12) representing a non-negative integer. Your task is to calculate the factorial of n. The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.',
-        'Test Case 1: 
-        Input: n = 5
-        Output: 120
-        Explanation: 5! = 5 * 4 * 3 * 2 * 1 = 120.
-        
-        Test Case 2: 
-        Input: n = 0
-        Output: 1
-        Explanation: 0! is defined as 1.',
+        'Test Case 1: Input: n = 5\nOutput: 120\nExplanation: 5! = 5 * 4 * 3 * 2 * 1 = 120.\n\nTest Case 2: Input: n = 0\nOutput: 1\nExplanation: 0! is defined as 1.',
         '0 ≤ n ≤ 12',
         'def calculate_factorial(n):\n    # return the answer',
         '#include <iostream>\nusing namespace std;\n\nint calculate_factorial(int n) {\n    // return the answer\n}',
         'def calculate_factorial(n):\n    if n == 0:\n        return 1\n    result = 1\n    for i in range(1, n + 1):\n        result *= i\n    return result',
         'int main() {\n    int n = 5;\n    cout << calculate_factorial(n) << endl;\n    n = 0;\n    cout << calculate_factorial(n) << endl;\n    return 0;\n}',
-        'n = 5\nprint(calculate_factorial(n))\nn = 0\nprint(calculate_factorial(n))'
+        'n = 5\nprint(calculate_factorial(n))\nn = 0\nprint(calculate_factorial(n))',
+        'public class Solution {\n    public static int calculateFactorial(int n) {\n        // return the answer\n    }\n}',
+        'public static void main(String[] args) {\n    int n = 5;\n    System.out.println(Solution.calculateFactorial(n));\n    n = 0;\n    System.out.println(Solution.calculateFactorial(n));\n}\n}'
     );
-    """)
+""")
 
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python, boiler_code_java, function_call_java)
     VALUES (
         'Palindrome Check',
         'You are given a string s. Your task is to determine whether s is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward. Ignore spaces, punctuation, and capitalization.',
-        'Test Case 1: 
-        Input: s = "racecar"
-        Output: True
-        Explanation: "racecar" is a palindrome.
-        
-        Test Case 2: 
-        Input: s = "hello"
-        Output: False
-        Explanation: "hello" is not a palindrome.',
+        'Test Case 1: Input: s = "racecar"\nOutput: True\nExplanation: "racecar" is a palindrome.\n\nTest Case 2: Input: s = "hello"\nOutput: False\nExplanation: "hello" is not a palindrome.',
         '1 ≤ len(s) ≤ 1000',
         'def is_palindrome(s):\n    # return the answer',
         '#include <iostream>\nusing namespace std;\n\nbool is_palindrome(string s) {\n    // return the answer\n}',
         'def is_palindrome(s):\n    s = s.lower().replace(" ", "").strip()\n    return s == s[::-1]',
         'int main() {\n    string s = "racecar";\n    cout << is_palindrome(s) << endl;\n    s = "hello";\n    cout << is_palindrome(s) << endl;\n    return 0;\n}',
-        's = "racecar"\nprint(is_palindrome(s))\ns = "hello"\nprint(is_palindrome(s))'
+        's = "racecar"\nprint(is_palindrome(s))\ns = "hello"\nprint(is_palindrome(s))',
+        'public class Solution {\n    public static boolean isPalindrome(String s) {\n        // return the answer\n    }\n}',
+        'public static void main(String[] args) {\n    String s = "racecar";\n    System.out.println(Solution.isPalindrome(s));\n    s = "hello";\n    System.out.println(Solution.isPalindrome(s));\n}\n}'
     );
-    """)
+""")
 
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python, boiler_code_java, function_call_java)
     VALUES (
         'Fibonacci Series',
         'You are given a single integer n (0 ≤ n ≤ 30). Your task is to generate the first n numbers in the Fibonacci sequence.',
-        'Test Case 1: 
-        Input: n = 5
-        Output: [0, 1, 1, 2, 3]
-        Explanation: The first 5 numbers in the Fibonacci sequence are [0, 1, 1, 2, 3].
-        
-        Test Case 2: 
-        Input: n = 8
-        Output: [0, 1, 1, 2, 3, 5, 8, 13]',
+        'Test Case 1: Input: n = 5\nOutput: [0, 1, 1, 2, 3]\nExplanation: The first 5 numbers in the Fibonacci sequence are [0, 1, 1, 2, 3].\n\nTest Case 2: Input: n = 8\nOutput: [0, 1, 1, 2, 3, 5, 8, 13]',
         '0 ≤ n ≤ 30',
         'def generate_fibonacci(n):\n    # return the answer',
         '#include <iostream>\n#include <vector>\nusing namespace std;\n\nvector<int> generate_fibonacci(int n) {\n    // return the answer\n}',
         'def generate_fibonacci(n):\n    fibonacci = [0, 1]\n    while len(fibonacci) < n:\n        next_number = fibonacci[-1] + fibonacci[-2]\n        fibonacci.append(next_number)\n    return fibonacci',
-        'int main() {\n    int n = 5;\n    vector<int> result = generate_fibonacci(n);\n    for (int num : result) {\n        cout << num << " ";\n    }\n    cout << endl;\n    n = 8;\n    result = generate_fibonacci(n);\n    for (int num : result) {\n        cout << num << " ";\n    }\n    cout << endl;\n    return 0;\n}',
-        'n = 5\nresult = generate_fibonacci(n)\nfor num in result:\n    print(num, end=" ")\nn = 8\nresult = generate_fibonacci(n)\nfor num in result:\n    print(num, end=" ")'
+        'int main() {\n    int n = 5;\n    vector<int> result = generate_fibonacci(n);\n    for (int num : result) {\n        cout << num << " ";\n    }\n    cout << endl;\n    n = 8;\n    result = generate_fibonacci(n);\n    for (int num : result) {\n        cout << num << " ";\n    }\n    cout << endl;\n}',
+        'n = 5\nresult = generate_fibonacci(n)\nfor num in result:\n    print(num, end=" ")\nn = 8\nresult = generate_fibonacci(n)\nfor num in result:\n    print(num, end=" ")',
+        'public class Solution {\n    public static List<Integer> generateFibonacci(int n) {\n        // return the answer\n    }\n}',
+        'public static void main(String[] args) {\n    int n = 5;\n    List<Integer> result = Solution.generateFibonacci(n);\n    for (int num : result) {\n        System.out.print(num + " ");\n    }\n    System.out.println();\n    n = 8;\n    result = Solution.generateFibonacci(n);\n    for (int num : result) {\n        System.out.print(num + " ");\n    }\n    System.out.println();\n}\n}'
     );
-    """)
-
+""")
 
     return redirect('/') 
 
@@ -199,37 +162,48 @@ def check_code():
     code = request.form['code']
     problem_id = request.form['problem_id']
     lang = request.form['lang']
+    type = request.form['type']
+
     problem_info = db.execute("SELECT * from problems WHERE problem_id = ?", problem_id)[0]
     boiler_code_cpp = problem_info['boiler_code_cpp']
     boiler_code_python = problem_info['boiler_code_python']
+    boiler_code_java = problem_info['boiler_code_java']
     function_call_cpp = problem_info["function_call_cpp"]
     function_call_python = problem_info["function_call_python"]
+    function_call_java = problem_info["function_call_java"]
+
     testing_code = problem_info["testing_code"]
 
-    if code == boiler_code_python or code == boiler_code_cpp:
+    if code == boiler_code_python or code == boiler_code_cpp or code == boiler_code_java:
         response = {
             "testcase_passed": False,
             "result": "Please enter code.",
         }
         return jsonify(response)        
 
-    # print(code +"\n"+ final)
-    print(testing_code +"\n"+ function_call_python)
+    # print(testing_code +"\n"+ function_call_python)
     try:
         if lang == "python":
             result,runtime = compile_python(code+"\n"+function_call_python)
         elif lang == "cpp":
             result,runtime = compile_cpp(code+"\n"+function_call_cpp)
+        elif lang == "java":
+            result,runtime = compile_java(code+"\n"+function_call_java)
 
         runtime = runtime * 1000  # Convert seconds to milliseconds and multiply by 1000
         runtime_formatted = "{:.2f}".format(runtime)  # Format to have 4 digits
-        testing_code_result = subprocess.check_output(['python', '-c', testing_code +"\n"+ function_call_python], stderr=subprocess.STDOUT, text=True)
+        
+        testcase_passed = False
+        
+        if type == "check":
+            testing_code_result = subprocess.check_output(['python', '-c', testing_code +"\n"+ function_call_python], stderr=subprocess.STDOUT, text=True)
+            testcase_passed = result.strip() == testing_code_result.strip()
         if runtime == 0:
             status = False
         else:
             status = True
+
         # Compare the results of the two executions
-        testcase_passed = result.strip() == testing_code_result.strip()
         # Return the result and testcase_passed as JSON
         response = {
             "testcase_passed": testcase_passed,
--- compiler.py
+++ compiler.py
@@ -4,7 +4,7 @@ import random
 import time
 import string
 
-temp_folder_name = 'CPP_TEMP'
+temp_folder_name = 'COMPILER_TEMP'
 current_directory = os.getcwd()
 temp_folder_path = os.path.join(current_directory, temp_folder_name)
 if not os.path.exists(temp_folder_path):
@@ -30,7 +30,7 @@ def compile_cpp(code):
                 os.remove(cpp_file_name)
             except:
                 pass
-            return (e.output,0)
+            return ("Runtime Error: " + e.output,0)
         # Execute the compiled program within the temporary folder
         executable_name = os.path.join(temp_folder_path, f"{random_file_name}.exe")
         try:
@@ -40,7 +40,7 @@ def compile_cpp(code):
             runtime = end_time-start_time
         except subprocess.CalledProcessError as e:
             runtime = 0
-            result = e.output
+            result = "Compile Time Error" + e.output
         try:
             os.remove(cpp_file_name)
         except:
@@ -58,6 +58,50 @@ def compile_python(code):
         end_time = time.time()
         runtime = end_time-start_time
     except subprocess.CalledProcessError as e:
-        result = e.output
+        result =  "Compile Time Error" + e.output
         runtime = 0
     return (result,runtime)
+
+def compile_java(code):
+    print(code)
+    # Generate a random file name within the temporary folder
+    random_file_name = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+    code = code.replace("Solution",random_file_name)
+    java_file_name = os.path.join(temp_folder_path, f"{random_file_name}.java")
+    
+    # Save the Java code to the random file
+    with open(java_file_name, "w") as java_file:
+        java_file.write(code)
+
+    # Compile the Java code to a class file within the temporary folder
+    compile_command = f"javac {java_file_name}"
+    try:
+        subprocess.check_output(compile_command, shell=True, stderr=subprocess.STDOUT, text=True, cwd=temp_folder_path)
+    except subprocess.CalledProcessError as e:
+        try:
+            os.remove(java_file_name)
+        except:
+            pass
+        return ("Runtime Error: " + e.output, 0)
+
+    # Execute the compiled Java program within the temporary folder
+    class_name = random_file_name
+    try:
+        start_time = time.time()
+        result = subprocess.check_output(f"java {class_name}", shell=True, stderr=subprocess.STDOUT, text=True, cwd=temp_folder_path)
+        end_time = time.time()
+        runtime = end_time - start_time
+    except subprocess.CalledProcessError as e:
+        runtime = 0
+        result = e.output
+
+    try:
+        os.remove(java_file_name)
+    except:
+        pass
+    try:
+        os.remove(f"{class_name}.class")
+    except:
+        pass
+
+    return (result, runtime)
