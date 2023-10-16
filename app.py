5610c2d11ab477ea3a7e09144823bb6619468bb5
= <anaskhaaan.28@gmail.com>
2023-10-16 17:15:46 +0530

    remove analytics

--- app.py
+++ app.py
@@ -21,9 +21,11 @@ db.execute("""
         problem_description TEXT,
         test_cases TEXT,
         constraints TEXT,
-        boiler_code TEXT,
+        boiler_code_python TEXT,
+        boiler_code_cpp TEXT,
         testing_code TEXT,
-        function_call_code TEXT
+        function_call_python TEXT,
+        function_call_cpp TEXT
     )
 """)
 
@@ -50,66 +52,68 @@ def log_request_info():
     
 @app.route('/')
 def index():
-    unique_visits = db.execute("SELECT COUNT(DISTINCT ip_address) FROM request_logs")[0]['COUNT(DISTINCT ip_address)'] + 50
     problems = db.execute("SELECT * from problems")
-    return render_template('index.html',problems=problems,unique_visits=unique_visits)
+    return render_template('index.html',problems=problems)
 
 @app.route('/create_sample')
 def create_sample():
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases,constraints,boiler_code, testing_code, function_call_code)
-VALUES (
-    'Sum of Squares',
-    'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of squares of all positive integers from 1 to n inclusive. 
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    VALUES (
+        'Sum of Squares',
+        'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of squares of all positive integers from 1 to n inclusive. 
     
-    Write a program to find this sum using the following formula: 
+        Write a program to find this sum using the following formula: 
     
-    Sum = 1² + 2² + 3² + ... + n²',
-    'Test Case 1: 
-    Input : n = 5
-    Output: 55,
-    Explanation: the first 5 positive integers (1² + 2² + 3² + 4² + 5²) which results in a sum of 55.
+        Sum = 1² + 2² + 3² + ... + n²',
+        'Test Case 1: 
+        Input : n = 5
+        Output: 55,
+        Explanation: the first 5 positive integers (1² + 2² + 3² + 4² + 5²) which results in a sum of 55.
 
-    Test Case 2: 
-    Input : n = 8
-    Output: 204',
-    '1 ≤ n ≤ 1000',
-    'def square(n):\n    # return the answer',
-    'def square(n):\n    total = 0\n    for i in range(1, n + 1):\n        total += i ** 2\n    return total',
+        Test Case 2: 
+        Input : n = 8
+        Output: 204',
+        '1 ≤ n ≤ 1000',
+        'def square(n):\n    # return the answer',
+        '#include <iostream>\nusing namespace std;\n\nint square(int n) {\n    // return the answer\n}',
+        'def square(n):\n    total = 0\n    for i in range(1, n + 1):\n        total += i ** 2\n    return total',
+        'int main() {\n    int n = 5;\n    cout << square(n) << endl;\n    n = 8;\n    cout << square(n) << endl;\n    return 0;\n}',
     'n = 5\n
 print(square(n))\n
 n = 8\n
 print(square(n))'
 );
-
 """)
-    db.execute("""INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code, testing_code, function_call_code)
-VALUES (
-    'Sum of Integers',
-    'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of all positive integers from 1 to n inclusive. 
-    
-    Write a program to find this sum using the following formula: 
-    
-    Sum = 1 + 2 + 3 + ... + n',
-    'Test Case 1: 
-    Input: n = 5
-    Output: 15
-    Explanation: The sum of integers from 1 to 5 is 1 + 2 + 3 + 4 + 5, which results in a sum of 15.
 
-    Test Case 2: 
-    Input: n = 8
-    Output: 36',
-    '1 ≤ n ≤ 1000',
-    'def calculate_sum(n):\n    # return the answer',
-    'def calculate_sum(n):\n    total = 0\n    for i in range(1, n + 1):\n        total += i\n    return total',
-    'n = 5\n
-print(calculate_sum(n))\n
-n = 8\n
-print(calculate_sum(n))'
-);
-""")
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases,constraints,boiler_code, testing_code, function_call_code)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
+    VALUES (
+        'Sum of Integers',
+        'You are given a single integer n (1 ≤ n ≤ 1000) representing the range. Your task is to calculate the sum of all positive integers from 1 to n inclusive. 
+        
+        Write a program to find this sum using the following formula: 
+        
+        Sum = 1 + 2 + 3 + ... + n',
+        'Test Case 1: 
+        Input: n = 5
+        Output: 15
+        Explanation: The sum of integers from 1 to 5 is 1 + 2 + 3 + 4 + 5, which results in a sum of 15.
+
+        Test Case 2: 
+        Input: n = 8
+        Output: 36',
+        '1 ≤ n ≤ 1000',
+        'def calculate_sum(n):\n    # return the answer',
+        '#include <iostream>\nusing namespace std;\n\nint calculate_sum(int n) {\n    // return the answer\n}',
+        'def calculate_sum(n):\n    total = 0\n    for i in range(1, n + 1):\n        total += i\n    return total',
+        'int main() {\n    int n = 5;\n    cout << calculate_sum(n) << endl;\n    n = 8;\n    cout << calculate_sum(n) << endl;\n    return 0;\n}',
+        'n = 5\nprint(calculate_sum(n))\nn = 8\nprint(calculate_sum(n))'
+    );
+    """)
+
+    db.execute("""
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
     VALUES (
         'Factorial Calculation',
         'You are given a single integer n (0 ≤ n ≤ 12) representing a non-negative integer. Your task is to calculate the factorial of n. The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.',
@@ -124,14 +128,15 @@ print(calculate_sum(n))'
         Explanation: 0! is defined as 1.',
         '0 ≤ n ≤ 12',
         'def calculate_factorial(n):\n    # return the answer',
+        '#include <iostream>\nusing namespace std;\n\nint calculate_factorial(int n) {\n    // return the answer\n}',
         'def calculate_factorial(n):\n    if n == 0:\n        return 1\n    result = 1\n    for i in range(1, n + 1):\n        result *= i\n    return result',
-        'n = 5\n
-print(calculate_factorial(n))\n
-n = 0\n
-print(calculate_factorial(n))');
-""")
+        'int main() {\n    int n = 5;\n    cout << calculate_factorial(n) << endl;\n    n = 0;\n    cout << calculate_factorial(n) << endl;\n    return 0;\n}',
+        'n = 5\nprint(calculate_factorial(n))\nn = 0\nprint(calculate_factorial(n))'
+    );
+    """)
+
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases,constraints,boiler_code, testing_code, function_call_code)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
     VALUES (
         'Palindrome Check',
         'You are given a string s. Your task is to determine whether s is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward. Ignore spaces, punctuation, and capitalization.',
@@ -146,14 +151,15 @@ print(calculate_factorial(n))');
         Explanation: "hello" is not a palindrome.',
         '1 ≤ len(s) ≤ 1000',
         'def is_palindrome(s):\n    # return the answer',
+        '#include <iostream>\nusing namespace std;\n\nbool is_palindrome(string s) {\n    // return the answer\n}',
         'def is_palindrome(s):\n    s = s.lower().replace(" ", "").strip()\n    return s == s[::-1]',
-        's = "racecar"\n
-print(is_palindrome(s))\n
-s = "hello"\n
-print(is_palindrome(s))');
-""")
+        'int main() {\n    string s = "racecar";\n    cout << is_palindrome(s) << endl;\n    s = "hello";\n    cout << is_palindrome(s) << endl;\n    return 0;\n}',
+        's = "racecar"\nprint(is_palindrome(s))\ns = "hello"\nprint(is_palindrome(s))'
+    );
+    """)
+
     db.execute("""
-    INSERT INTO problems (problem_name, problem_description, test_cases,constraints,boiler_code, testing_code, function_call_code)
+    INSERT INTO problems (problem_name, problem_description, test_cases, constraints, boiler_code_python, boiler_code_cpp, testing_code, function_call_cpp, function_call_python)
     VALUES (
         'Fibonacci Series',
         'You are given a single integer n (0 ≤ n ≤ 30). Your task is to generate the first n numbers in the Fibonacci sequence.',
@@ -167,21 +173,21 @@ print(is_palindrome(s))');
         Output: [0, 1, 1, 2, 3, 5, 8, 13]',
         '0 ≤ n ≤ 30',
         'def generate_fibonacci(n):\n    # return the answer',
+        '#include <iostream>\n#include <vector>\nusing namespace std;\n\nvector<int> generate_fibonacci(int n) {\n    // return the answer\n}',
         'def generate_fibonacci(n):\n    fibonacci = [0, 1]\n    while len(fibonacci) < n:\n        next_number = fibonacci[-1] + fibonacci[-2]\n        fibonacci.append(next_number)\n    return fibonacci',
-        'n = 5\n
-print(generate_fibonacci(n))\n
-n = 8\n
-print(generate_fibonacci(n))');
-""")
+        'int main() {\n    int n = 5;\n    vector<int> result = generate_fibonacci(n);\n    for (int num : result) {\n        cout << num << " ";\n    }\n    cout << endl;\n    n = 8;\n    result = generate_fibonacci(n);\n    for (int num : result) {\n        cout << num << " ";\n    }\n    cout << endl;\n    return 0;\n}',
+        'n = 5\nresult = generate_fibonacci(n)\nfor num in result:\n    print(num, end=" ")\nn = 8\nresult = generate_fibonacci(n)\nfor num in result:\n    print(num, end=" ")'
+    );
+    """)
+
 
     return redirect('/') 
 
 
 @app.route('/problems/<problem_id>')
 def problem_view(problem_id):
-    unique_visits = db.execute("SELECT COUNT(DISTINCT ip_address) FROM request_logs")[0]['COUNT(DISTINCT ip_address)'] + 50
     problem_info = db.execute("SELECT * from problems WHERE problem_id = ?", problem_id)[0]
-    return render_template('questions.html',problem_info=problem_info,unique_visits=unique_visits)
+    return render_template('questions.html',problem_info=problem_info)
 
 @app.route('/test')
 def test():
@@ -194,34 +200,36 @@ def check_code():
     problem_id = request.form['problem_id']
     lang = request.form['lang']
     problem_info = db.execute("SELECT * from problems WHERE problem_id = ?", problem_id)[0]
-    boiler_code = problem_info['boiler_code']
-    if code == boiler_code:
+    boiler_code_cpp = problem_info['boiler_code_cpp']
+    boiler_code_python = problem_info['boiler_code_python']
+    function_call_cpp = problem_info["function_call_cpp"]
+    function_call_python = problem_info["function_call_python"]
+    testing_code = problem_info["testing_code"]
+
+    if code == boiler_code_python or code == boiler_code_cpp:
         response = {
             "testcase_passed": False,
             "result": "Please enter code.",
         }
         return jsonify(response)        
 
-    final = problem_info["function_call_code"]
-    code2 = problem_info["testing_code"]
-
     # print(code +"\n"+ final)
+    print(testing_code +"\n"+ function_call_python)
     try:
-        if lang == "py":
-            result,runtime = compile_python(code+"\n"+final)
+        if lang == "python":
+            result,runtime = compile_python(code+"\n"+function_call_python)
         elif lang == "cpp":
-            result,runtime = compile_cpp(code)
-        print(result)
+            result,runtime = compile_cpp(code+"\n"+function_call_cpp)
+
         runtime = runtime * 1000  # Convert seconds to milliseconds and multiply by 1000
         runtime_formatted = "{:.2f}".format(runtime)  # Format to have 4 digits
-        result2 = subprocess.check_output(['python', '-c', code2 +"\n"+ final], stderr=subprocess.STDOUT, text=True)
+        testing_code_result = subprocess.check_output(['python', '-c', testing_code +"\n"+ function_call_python], stderr=subprocess.STDOUT, text=True)
         if runtime == 0:
             status = False
         else:
             status = True
         # Compare the results of the two executions
-        testcase_passed = result.strip() == result2.strip()
-
+        testcase_passed = result.strip() == testing_code_result.strip()
         # Return the result and testcase_passed as JSON
         response = {
             "testcase_passed": testcase_passed,
